d501a23b3246d09352fde015932f60c8c43c6f7d
Kristaps Kurmis <kristapskurmis@Kristapss-MacBook-Air.local>
2022-10-12 21:31:22 +0300

    working with nordigen package

--- apps/api/views.py
+++ apps/api/views.py
@@ -2,27 +2,36 @@ import json
 from django.shortcuts import render
 from django.http import HttpResponse
 # from tasks import fetch_banks
+from nordigen import NordigenClient
 import requests
 
 from nordigen_api.settings import USER_SECRET_ID, USER_SECRET_KEY
 
+client = NordigenClient(
+    secret_id=USER_SECRET_ID,
+    secret_key=USER_SECRET_KEY
+)
+
+token_data = client.generate_token()
+
+
 def index(request: str) -> str:
-    token = get_token()
+    institutions = client.institution.get_institutions()
 
-    return render(request, 'index.html', {'banks': get_banks(token)})
+    return render(request, 'index.html', {'banks': institutions})
 
-def get_banks(token: str) -> json:
-    api_url = 'https://ob.nordigen.com/api/v2/institutions/'
+# def get_token() -> str:
+#     api_url = 'https://ob.nordigen.com/api/v2/token/new/'
+
+#     response = requests.post(api_url, data = {'secret_id': USER_SECRET_ID, 'secret_key': USER_SECRET_KEY})
+#     response_json = response.json()
+
+#     return response_json['access']
+
+# def get_banks(token: str) -> json:
+#     api_url = 'https://ob.nordigen.com/api/v2/institutions/'
     
-    response = requests.get(api_url, headers = {'Authorization': 'Bearer ' + token})
-    response_json = json.dumps(response.text)
+#     response = requests.get(api_url, headers = {'Authorization': 'Bearer ' + token})
+#     response_json = json.dumps(response.text)
 
-    return response_json
-
-def get_token() -> str:
-    api_url = 'https://ob.nordigen.com/api/v2/token/new/'
-
-    response = requests.post(api_url, data = {'secret_id': USER_SECRET_ID, 'secret_key': USER_SECRET_KEY})
-    response_json = response.json()
-
-    return response_json['access']
+#     return response_json
