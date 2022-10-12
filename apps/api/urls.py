7b967d6f01c38acf14e9f3b0dec1a7e116d2da52
Kristaps Kurmis <kristapskurmis@Kristapss-MacBook-Air.local>
2022-10-12 23:07:59 +0300

    auth to bank

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index),
+    path('index/', views.index),    
+    path('index/auth/<str:institution_id>', views.auth),
+    path('index/details', views.auth),
 ]
--- apps/api/views.py
+++ apps/api/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # from tasks import fetch_banks
 from nordigen import NordigenClient
+from uuid import uuid4
+from django.shortcuts import redirect
 import requests
 
 from nordigen_api.settings import USER_SECRET_ID, USER_SECRET_KEY
@@ -14,11 +16,30 @@ client = NordigenClient(
 
 token_data = client.generate_token()
 
-
-def index(request: str) -> str:
+def index(request: str):
     institutions = client.institution.get_institutions()
 
     return render(request, 'index.html', {'banks': institutions})
+
+def auth(request: str, institution_id: str):
+    init = client.initialize_session(
+        institution_id=institution_id,
+        redirect_uri="http://127.0.0.1:8000/index/details",
+        reference_id=str(uuid4())
+    )
+
+    accounts = client.requisition.get_requisition_by_id(
+        requisition_id=init.requisition_id
+    )
+
+    link = init.link
+    requisition_id = init.requisition_id
+
+    return redirect(link)
+
+def details(request: str):
+
+    return()
 
 # def get_token() -> str:
 #     api_url = 'https://ob.nordigen.com/api/v2/token/new/'
