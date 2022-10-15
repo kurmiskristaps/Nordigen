2fd83f1724978ff17de6acf612e6f5135bb4415f
Kristaps Kurmis <kristapskurmis@Kristapss-MacBook-Air.local>
2022-10-15 13:36:40 +0300

    error handling

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 from django.http import HttpResponse
 from nordigen import NordigenClient
@@ -14,29 +15,60 @@ token_data = client.generate_token()
 
 
 def index(request: str):
-    institutions = client.institution.get_institutions()
-
+    try:
+        institutions = client.institution.get_institutions()
+    except Exception:
+        return render(request, 'error.html')
+        
     return render(request, 'index.html', {'banks': institutions})
 
 
 def auth(request: str, institution_id: str):
     if (institution_id):
-        init = client.initialize_session(
-            institution_id=institution_id,
-            redirect_uri="http://localhost:8000/index/details",
-            reference_id=str(uuid4())
-        )
+        try:
+            init = client.initialize_session(
+                institution_id=institution_id,
+                redirect_uri="http://localhost:8000/index/details",
+                reference_id=str(uuid4())
+            )
 
-        link = init.link
-        request.session['req_id'] = init.requisition_id
+            link = init.link
+            request.session['req_id'] = init.requisition_id
+        except Exception:
+            return render(request, 'error.html')
 
         return redirect(link)
     return redirect('/index')
 
 def details(request: str):
-    accounts = client.requisition.get_requisition_by_id(
-        requisition_id = request.session['req_id']
-    )
-    request.session['ref'] = request.GET.get('ref', '')
+    if len(request.session['req_id']) == 0:
+        return redirect('/index')
 
-    return HttpResponse(accounts)
+    try:
+        accounts = client.requisition.get_requisition_by_id(
+            requisition_id = request.session['req_id']
+        )
+
+        request.session['ref'] = request.GET.get('ref')
+
+        accounts_data = []
+
+        for id in accounts['accounts']:
+            account = client.account_api(id)
+            metadata = account.get_metadata()
+            transactions = account.get_transactions()
+            details = account.get_details()
+            balances = account.get_balances()
+
+            accounts_data.append(
+                {
+                    "metadata": metadata,
+                    "details": details,
+                    "balances": balances,
+                    "transactions": transactions,
+                }
+            )
+    except Exception:
+        return render(request, 'error.html')
+
+    return HttpResponse(accounts_data)
